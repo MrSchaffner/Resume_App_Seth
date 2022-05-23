18c1e6015372b905a028ae6453663c7a6a9b1c26
GenoSophy <62560010+MrSchaffner@users.noreply.github.com>
2022-05-23 13:30:12 -0700

    iniy

---
+++ app/src/main/java/cowburger/media/resume_app_seth/MainActivity.java
@@ -0,0 +1,13 @@
+package cowburger.media.resume_app_seth;
+
+import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+}
